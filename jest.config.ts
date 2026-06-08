1fd8019e3c02d2f0f3929c0d5a961dc56851828d
Akshat Garg <akshat.garg@salesforce.com>
2026-06-08 16:58:00 +0530

    feat: jest + testing library setup

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config: Config = {
+  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+}
+
+export default createJestConfig(config)
